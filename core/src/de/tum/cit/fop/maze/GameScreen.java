a8fd32344965c1366634d52b3ac18c4e584f1d30
Matei Tudose <matei@mateitudose.dev>
2025-01-03 13:17:49 +0200

    Restart project + add Box2D

--- core/src/de/tum/cit/fop/maze/GameScreen.java
+++ core/src/de/tum/cit/fop/maze/GameScreen.java
@@ -2,65 +2,58 @@ package de.tum.cit.fop.maze;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GameScreen implements Screen {
-
     private final MazeRunnerGame game;
     private final OrthographicCamera camera;
     private final ScreenViewport viewport;
-    private final BitmapFont font;
-    private final MainCharacter mainCharacter;
+    private final World gameWorld;
 
     public GameScreen(MazeRunnerGame game) {
         this.game = game;
-        this.mainCharacter = new MainCharacter();
         camera = new OrthographicCamera();
         viewport = new ScreenViewport(camera);
         camera.setToOrtho(false);
-        camera.zoom = 1f;
-        font = game.getSkin().getFont("font");
-    }
-
-    @Override
-    public void render(float delta) {
-        mainCharacter.update(delta, game);
-        ScreenUtils.clear(0, 0, 0, 1);
-        camera.update();
-        SpriteBatch batch = game.getSpriteBatch();
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        mainCharacter.render(batch, delta);
-        batch.end();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        camera.setToOrtho(false);
-        viewport.update(width, height);
-        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
-    }
-
-    @Override
-    public void pause() {
-    }
-
-    @Override
-    public void resume() {
+        Box2D.init();
+        gameWorld = new World(new Vector2(0, -10), true);
     }
 
     @Override
     public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+        gameWorld.step(1/60f, 6, 2);
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
     }
 
     @Override
     public void hide() {
+
     }
 
     @Override
     public void dispose() {
+
     }
 }
---
+++ core/src/de/tum/cit/fop/maze/Player.java
@@ -0,0 +1,5 @@
+package de.tum.cit.fop.maze;
+
+public class Player {
+
+}
